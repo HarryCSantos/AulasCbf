d1f8edc0a8bd4a131127347c4bfd06dbd70f31b2
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-01-24 19:19:02 -0300

    reo

---
+++ Aula03/Aula03.cs
@@ -0,0 +1,21 @@
+using System;
+class Aula03{
+    static void Main(){
+        byte n1=10; // 0 e 255
+
+        int num1,num2;
+
+        num1 = 10;
+        num2 = 2;
+
+        char letra='8';
+        float valor=5.3f;
+        string nome="Sophia";
+
+        var aux=nome;
+
+        Console.WriteLine("Valor da Variável: " + aux + " is a worderful woman!");
+
+        Console.WriteLine("A multiplicação de " + num1 + " vezes " + num2 + " é igual a: " + num1*num2);
+    }
+}
