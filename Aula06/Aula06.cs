24ce399a63381942b23d3b2b182f238e78855608
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-01-26 19:29:32 -0300

    cfb

---
+++ Aula06/Aula06.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Aula05{
+
+    static void Main(){
+
+        int n1,n2,n3;
+
+        n1=10; n2=20; n3=30;
+
+        Console.WriteLine(n1 + "," + n2 + "," + n3));
+
+    }    
