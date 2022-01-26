5053e6f5c48848e63efd0580dd9df9a5ad83eaf1
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-01-26 19:36:44 -0300

    exa

--- Aula06/Aula06.cs
+++ Aula06/Aula06.cs
@@ -8,6 +8,7 @@ class Aula05{
 
         n1=10; n2=20; n3=30;
 
-        Console.WriteLine(n1 + "," + n2 + "," + n3));
+        Console.WriteLine("n1={0}, n2={1}, n3={2}", n1,n2,n3);
 
     }    
+}  
