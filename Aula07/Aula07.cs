ac3c19564ae562e961f261619797c840a9378be1
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-01-27 20:45:28 -0300

    chan

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,9 @@
+using System;
+class Aula07{
+    static void Main(){
+        const string canal="CFB Curso";
+        const double pi=3.1415;
+
+        Console.WriteLine("Canal {0}",canal);
+    }
+}
