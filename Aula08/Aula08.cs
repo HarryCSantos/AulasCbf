24f07b2ea2eb1731972d0c0b922dc56e8b290273
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-01-28 19:10:30 -0300

    alt

---
+++ Aula08/Aula08.cs
@@ -0,0 +1,10 @@
+using System;
+class Aula08{
+    static void Main(){
+        int v1,v2,soma;
+        string nome;
+        Console.Write("Digite seu nome: ");
+        nome=Console.ReadLine();
+        Console.WriteLine("Nome digitado: {0}",nome);
+    }
+}
