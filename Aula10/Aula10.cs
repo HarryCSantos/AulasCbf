56cbebba1f811bd3a11415690b409c0d00e2ebf1
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-01-31 20:24:30 -0300

    alt

--- Aula10/Aula10.cs
+++ Aula10/Aula10.cs
@@ -3,7 +3,7 @@ class Aula10{
     
     enum DiasSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado};
     static void Main(){
-            DiasSemana ds = DiasSemana.Domingo;
+            DiasSemana ds = (DiasSemana)3;
             Console.WriteLine(ds);
             
     }
