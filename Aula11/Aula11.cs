8a895d7cb8fcace5c206d4f6999183ae96a0a177
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-03 22:26:44 -0300

    alt

--- Aula11/Aula11.cs
+++ Aula11/Aula11.cs
@@ -2,8 +2,8 @@ using System;
 class Aula11{
     static void Main(){
 
-        int n1=10;
-        float n2=n1;
+        float n1=10.5f;
+        int n2=(int)n1;//type cast
 
         Console.WriteLine(n2);
     }
