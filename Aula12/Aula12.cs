9098fdacd5d436c8e81585d500de65f87d5aa129
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-05 23:24:19 -0300

    alt

---
+++ Aula12/Aula12.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Aula12{
+
+    static void Main(){
+
+        //Se(Expressão_Lógica V) {executa a ação}
+        
+        int nota=80;
+
+        if(nota >=60){
+            Console.WriteLine("Aprovado");
+
+        }
+
+
+    }
+}
