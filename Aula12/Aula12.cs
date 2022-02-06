439c3b11ec0ba9a14f47b9372a0521b104daea48
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-06 05:43:46 -0300

    alt

--- Aula12/Aula12.cs
+++ Aula12/Aula12.cs
@@ -6,32 +6,30 @@ class Aula12{
 
         //IF-->Se(Expressão_Lógica V) {executa a ação}
         
-        int n1,n2,n3,n4;
-        n1=n2=n3=n4=0;
+        int n1,n2,n3,n4,res;
+        res=n1=n2=n3=n4=0;
+        string resultado="Reprovado";
 
         Console.WriteLine("Digite a nota 1: ");
         n1=int.Parse(Console.ReadLine());
 
         Console.WriteLine("Digite a nota 2: ");
-        n1=int.Parse(Console.ReadLine());
+        n2=int.Parse(Console.ReadLine());
 
         Console.WriteLine("Digite a nota 3: ");
-        n1=int.Parse(Console.ReadLine());
+        n3=int.Parse(Console.ReadLine());
 
         Console.WriteLine("Digite a nota 4: ");
-        n1=int.Parse(Console.ReadLine());
+        n4=int.Parse(Console.ReadLine());
 
+        res=n1+n2+n3+n4;
 
-        string resultado="Reprovado";
-
-        nota=int.Parse(Console.ReadLine());
-
-        if(nota >=60){
+        if(res >=60){
             resultado="Aprovado";
 
         }
 
-        Console.WriteLine("Resultado:{0}",resultado);
+        Console.WriteLine("Nota: {0} - Resultado: {1}",res,resultado);
 
     }
 }
