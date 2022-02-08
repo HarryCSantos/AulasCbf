dd43be28ffaee519c5b082aa465e0e5a8ea7146c
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-07 21:50:39 -0300

    alt

---
+++ Aula14/Aula14.cs
@@ -0,0 +1,48 @@
+using System;
+
+class Aula12{
+
+    static void Main(){
+
+        //IF-->Se(Expressão_Lógica V) {executa a ação}
+        
+        int n1,n2,n3,n4,res;
+        res=n1=n2=n3=n4=0;
+        string resultado; 
+
+        Console.WriteLine("Digite a nota 1: ");
+        n1=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 2: ");
+        n2=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 3: ");
+        n3=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 4: ");
+        n4=int.Parse(Console.ReadLine());
+
+        res=n1+n2+n3+n4;
+
+        //>=60 - Aprovado
+        //59 e 40 - Recuperação
+        //<40 - Reprovado
+
+        if(res >= 60){
+            if(res >= 90){
+                resultado="Aprovado com louvor";
+            }else{
+                resultado="Aprovado";
+            }
+        }else{
+            if(res >= 40{
+                resultado="Recuperação";
+            }
+        }
+            
+
+        Console.WriteLine("Nota: {0} - Resultado: {1}",res,resultado);
+
+    }
+}
+
