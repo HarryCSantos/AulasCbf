cbec57cbd2bd840261bb40aea013d68b89004e36
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-09 08:23:02 -0300

    alt

--- Aula15/Aula15.cs
+++ Aula15/Aula15.cs
@@ -12,22 +12,22 @@ class Aula15{
     escolha=char.Parse(Console.ReadLine());
 
     switch (escolha){
-        case 'a':
-            tempo=50;
-            break;
-        case 'c':
-            tempo=480;
-            break;
-        case 'o': 
-            tempo=660;
-            break;
-        default:
-            tempo=-1;
-            break;
+            case 'a':
+                tempo=50;
+                break;
+            case 'c':
+                tempo=480;
+                break;
+            case 'o': 
+                tempo=660;
+                break;
+            default:
+                tempo=-1;
+                break;
         }
+
         if(tempo<0){
             Console.WriteLine("Transporte indisponível");
         }else{
             Console.WriteLine("Para o transporte escolhido o tempo é: {0} minutos");
         }
-}  
