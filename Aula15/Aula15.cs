c66b7551b53f21657598b5eb8713940113cd3948
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-08 23:43:27 -0300

    alt

--- Aula15/Aula15.cs
+++ Aula15/Aula15.cs
@@ -4,6 +4,7 @@ class Aula15{
     static void Main(){
 
         Console.WriteLine("Belo Horizonte/MG a Vitória/ES");
+        Console.WriteLine("Escolha o Transporte:[a]=Avião | [c]Carro | [o]Ônibus");
 
         switch (){
             
