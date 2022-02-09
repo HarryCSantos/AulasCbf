4bf771d9cee8b47945d3a8bded8d7738b8da9057
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-08 23:38:50 -0300

    alt

---
+++ Aula15/Aula15.cs
@@ -0,0 +1,13 @@
+using System;
+class Aula15{
+    {
+    static void Main(){
+
+        Console.WriteLine("Belo Horizonte/MG a Vitória/ES");
+
+        switch (){
+            
+        }
+    
+    }
+}
