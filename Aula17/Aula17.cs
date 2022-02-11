fd1c37afae534a3b0d6a285eeb9f4a1126d87cfd
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-11 06:21:24 -0300

    alt

---
+++ Aula17/Aula17.cs
@@ -0,0 +1,14 @@
+using Sustem;
+class Aula17
+{
+    static void Main()
+    {
+        int n1,n2,n3,n4,n5;
+        int[] n=new int[5];
+
+        n[0] = 111;
+        n[1] = 222;
+        n[2] = 333;
+        n[3] = 555;
+    }
+}
