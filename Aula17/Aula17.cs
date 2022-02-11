882b04eb8d5ca584f8606bb38c6cf7723278bfb6
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-11 06:29:29 -0300

    alt

--- Aula17/Aula17.cs
+++ Aula17/Aula17.cs
@@ -4,13 +4,14 @@ class Aula17
     static void Main()
     {
         int n1,n2,n3,n4,n5;
-        int[] n=new int[5];
+        int[] n= new int[5];
+        int[] num= new int[3] {55,77,99};
 
         n[0] = 111;
         n[1] = 222;
         n[2] = 333;
         n[3] = 555;
 
-        Console.WriteLine(n[0]);
+        Console.WriteLine(num[2]);
     }
 }
