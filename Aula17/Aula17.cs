31a38fce970ccc51bcb88de4eb65a9774a566bb9
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-11 08:19:49 -0300

    alt

--- Aula17/Aula17.cs
+++ Aula17/Aula17.cs
@@ -1,22 +1,15 @@
 using System;
 class Aula17
 {
-    static void Main()
-    {
-        //int n1,n2,n3,n4,n5;
-        int[] n= new int[5];
-        int[] num= new int[3] {55,77,99,88};
-        //int[] num={55,77,99},66,88;
-        string[] veiculos=new string[3];
-
-        veiculo[0]="Carro";
-        veiculo[1]="Avião";
-        veiculo[2]="Navio";
-        veiculo[3]="Moto";
-        veiculo[4]="Ônibus";
-        
-         
-
-        Console.WriteLine(veiculo[2]);
-    }
+   static void Main()
+   {
+       string[] pilotos = new string[4] {"Ayrton Senna", "Michael Schumacher", "Lewis Hamilton", "Alan Prost"};
+       Console.WriteLine(pilotos[3]);
+       Console.WriteLine();
+       pilotos[3] = "Rubens Barrichello";
+       foreach (string piloto in pilotos)
+       {
+           Console.WriteLine(piloto);
+       }
+   }
 }
