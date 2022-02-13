f177191daee1cff996a1cb68378eed53897926bc
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-13 18:42:48 -0300

    alt

---
+++ Aula18/Aula18.cs
@@ -0,0 +1,15 @@
+using System;
+class Aula18
+{
+    sttic void Main()
+        {
+            int[,] n=new int[3,5];
+
+            n[0]=11;
+            n[1]=22;
+            n[2]=33;
+            n[3]=44;
+            n[4]=55;
+        }
+    
+}
