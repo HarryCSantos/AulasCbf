6d6af5097b14f321e0793c058e30df9651b84111
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-14 18:47:21 -0300

    atl

--- Aula19/Aula19.cs
+++ Aula19/Aula19.cs
@@ -2,12 +2,10 @@ using System;
 class Aula19
 {
     static void Main()
-    {
-        int num;
-        
+    {    
         for(int num=0;num<10;num++)
         {
-
+           Console.WriteLine("CFB Cursos"); 
         }
     }
 }
