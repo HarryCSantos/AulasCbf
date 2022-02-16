22280023db16109d1ec93fc29f36018a2fc3f937
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-15 21:08:47 -0300

    alt

--- Aula20/Aula20.cs
+++ Aula20/Aula20.cs
@@ -10,8 +10,8 @@ class Aula20
         while (i<num.Length)
         {
             num[i]=0;
-            i++;
             Console.WriteLine(num[i]);
+            i++;
         }
         Console.WriteLine("Fim do loop");
     }
