76f095f0a908f5088a5f6462c10d80f762fa172c
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-16 21:47:30 -0300

    alt

--- Aula21/Aula21.cs
+++ Aula21/Aula21.cs
@@ -4,9 +4,13 @@ class Aula21
     static void Main()
     {
         
+        string senha="123";
+        string senhauser;
+
         do
         {
-            
+            Console.WriteLine("Digite a senha: ");
+            senhauser=Console.ReadLine();
         }
         while(num<5);
     }
