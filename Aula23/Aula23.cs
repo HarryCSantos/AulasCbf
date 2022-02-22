f93190938ea55ba1baed054811bebd4d438df8be
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-21 21:17:11 -0300

    alt

--- Aula23/Aula23.cs
+++ Aula23/Aula23.cs
@@ -3,6 +3,9 @@ class Aula23
 {
     static void Main()
     {
-        int[] vetor1=new int[];
+        int[] vetor1=new int[5];
+        int[] vetor2=new int[5];
+        int[] vetor3=new int[5];
+        int[,] matriz=new int[2,5]
     }
 }
