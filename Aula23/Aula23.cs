4e5048d66ff7df3350a570d888029487b191b817
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-23 22:08:27 -0300

    alt

--- Aula23/Aula23.cs
+++ Aula23/Aula23.cs
@@ -100,6 +100,17 @@ class Aula23
             {
                 Console.WriteLine(n);
             }
+
+        // public static void Sort(array);
+        Array.Sort(vetor1);
+        Array.Sort(vetor2);
+        Array.Sort(vetor3);
+        Console.WriteLine("Vetor1");
+        foreach(int n in vetor1)
+        {
+            Console.WriteLine(n);
+        }
+
         }
 
 
