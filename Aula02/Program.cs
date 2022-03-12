f3134de5dc8743d8a0f5e63f8a23adf5b17f155c
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-11 22:06:39 -0300

    alt

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -9,7 +9,7 @@ namespace Aula02
              
             int = idade, idade = 32;
 
-        Console.WriteLine("Executando Aula 01, Curso CFB")
+        Console.WriteLine("Executando Aula 01, Curso CFB");
         }    
     }
 }
