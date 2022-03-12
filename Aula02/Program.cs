4dc7bc99d002c992cf08c3764a9725c36f471857
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-11 22:39:53 -0300

    alt

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -14,7 +14,7 @@ namespace Aula02
           ano = 12;
             
 
-        Console.WriteLine("Você possui " + meses + " de estudos" + " em breve completará " + ano " meses o que significa um ano.");
+        Console.WriteLine("Você possui " + meses + " de estudos" + " em breve completará " + ano + " meses o que significa um ano.");
         }    
     }
 }
